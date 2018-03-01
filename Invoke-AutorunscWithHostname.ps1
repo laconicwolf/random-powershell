633bd02d675642404f5b49d182122e2d2a04c4df
Jake Miller <laconicwolf@gmail.com>
2018-03-01 09:49:18 -0600

    Add files via upload

--- Invoke-AutorunscWithHostname.ps1
+++ Invoke-AutorunscWithHostname.ps1
@@ -24,7 +24,7 @@ Function Edit-AutorunsCsv {
     )
 
     $reader = [System.IO.File]::OpenText($InputCsvFilename)
-    $writer = New-Object System.IO.StreamWriter 'out2.csv'
+    $writer = New-Object System.IO.StreamWriter $OutputCsvFilename
     $counter = 1
     $targetHost = [system.environment]::MachineName
     for(;;) {
@@ -34,18 +34,12 @@ Function Edit-AutorunsCsv {
         }
         $data = $line.Split(",")
         if ($counter -eq 1) {
-            $writer.WriteLine('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},
-                              {10},{11},{12},{13},{14},{15},{16}', 'Hostname', 
-                              $data[0], $data[1], $data[2], $data[3], $data[4],
-                              $data[5],$data[6],$data[7],$data[8],$data[9],$data[10],
-                              $data[11],$data[12],$data[13],$data[14],$data[15],$data[16])
+            $writer.WriteLine('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17}', 
+                              'Hostname',$data[0],$data[1],$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],$data[11],$data[12],$data[13],$data[14],$data[15],$data[16])
         }
         else {
-            $writer.WriteLine('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},
-                              {10},{11},{12},{13},{14},{15},{16}', 
-                              $targetHost,$data[0],$data[1],$data[2],$data[3],$data[4],
-                              $data[5],$data[6],$data[7],$data[8],$data[9],$data[10],
-                              $data[11],$data[12],$data[13],$data[14],$data[15],$data[16])
+            $writer.WriteLine('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17}', 
+                              $targetHost,$data[0],$data[1],$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],$data[11],$data[12],$data[13],$data[14],$data[15],$data[16])
         }
         $counter += 1
     }
@@ -55,4 +49,4 @@ Function Edit-AutorunsCsv {
 
 
 Start-AutoRuns -FilePath C:\QT\bin\autorunsc.exe
-Edit-AutorunsCsv -InputCsvFilename C:\QT\bin\autoruns.csv -OutputCsvFilename autoruns_with_hostname.csv
+Edit-AutorunsCsv -InputCsvFilename C:\QT\bin\autoruns.csv -OutputCsvFilename C:\QT\bin\autoruns_with_hostname1.csv
